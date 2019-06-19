397ede115a3e3a62b3ba461eaf9e7f0c6a2ab3aa
Glitch (hello-express) <none>
2019-06-19 07:19:06 +0000

    🕴🌗 Checkpoint ./komutlar/slots.js:1246538/52

--- komutlar/slots.js
+++ komutlar/slots.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const { Command } = require('discord.js-commando');
 const { stripIndents } = require('common-tags');
 const slots = ['🍇', '🍊', '🍐', '🍒', '🍋'];
 
