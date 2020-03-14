a12044df052ac7aaa5c85e662c1d45a12cf41371
Glitch (first-enormous-microwave) <none>
2020-03-14 10:56:19 +0000

    😹🍎 Checkpoint ./komutlar/denene.js:7250430/495

--- komutlar/denene.js
+++ komutlar/denene.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
       tagdakiler = tagdakiler+1
     }
   })
-  message.channel.send("")
+  message.channel.send("<a:kus:688079319121133583> Black Deneme Yetkilisi Bilgi \n\n<a:ok:688079594821255201> 2 Günlük Bir Deneme Süreciniz. \n\n<a:ok:688079594821255201> 2 Gün İçerisinde 10 Saat  Mikrofonunuz Açık Seslide Durmanız Lazım \n\n<a:ok:688079594821255201> 2 Gün İçerisinde Metin Kanallarına 400 Adet Mesaj Yazmanız Gerek \n\n<a:ok:688079594821255201> 2 Gün İçerisindr Bunları YapmaSanız Yetkiniz Alınır. \n\n@everyone")
   const Discord = require('discord.js');
 
 
@@ -21,7 +21,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'tagsay',
+  name: 'denemnxndjxxjjxe',
   description: 'Tagdaki Kullanıcıları Sayar!',
   usage: 'Tag Sayar Fyukas'
 };   
