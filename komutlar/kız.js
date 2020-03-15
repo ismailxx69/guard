44ca3c87e4dbfd59920f8182d01c02b77d2fd659
Glitch (first-enormous-microwave) <none>
2020-03-15 11:12:53 +0000

    🤘🛀 Checkpoint ./komutlar/kız.js:7250430/6022 ./komutlar/erkek.js:7250430/5556

--- komutlar/kız.js
+++ komutlar/kız.js
@@ -1,19 +1,19 @@
-  const Discord = require('discord.js');
+const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
 exports.run = async (client, message, args) => {
 
-if(!message.member.roles.has("688056135336460307")) return message.reply(`Bu komutu kullanabilmen için <@&688056135336460307> yetkiye sahip olman lazım.`);
+if(!message.member.roles.has("688527280611262466")) return message.reply(`Bu komutu kullanabilmen için <@&688527280611262466> yetkiye sahip olman lazım.`);
   let kullanıcı = message.mentions.users.first()
   if (!kullanıcı) return message.channel.send('Hey sen birini kayıt etmen için birisini etiketlemelisin bunu gözden kaçırma!')
   let rol = message.mentions.roles.first()
   let member = message.guild.member(kullanıcı)
   member.addRole('688528910304280894')
-  member.removeRole('687744853647425553')
+  member.removeRole('688527158016081989')
   let embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-  .addField(`<a:kus:688079319121133583> Kadın olarak kayıt edilmiştir.`, `<a:kus:688079319121133583> Etiketlediğiniz Kişiden Başarıyla <@&687744968873345076> rolü verilmiştir.
-  <a:kus:688079319121133583> Etiketlediğiniz kişiden başarıyla <@&687744853647425553> rolü başarıyla alınmıştır.`)
+  .addField(`<a:kus:688079319121133583> Kız olarak kayıt edilmiştir.`, `<a:kus:688079319121133583> Etijetlediğiniz Kişiden başarıyla <@&688528910304280894> rolü verilmiştir.
+  <a:kus:688079319121133583> Etiketlediğiniz kişiden başarıyla <@&688527158016081989> rolü başarıyla alınmıştır.`)
   .setThumbnail(client.user.avatarURL)
   .setFooter(`Komutu kullanan yetkili : ${message.author.username}`)
   return message.channel.send(embed)
