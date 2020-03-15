5ae8bc4f6b10e1b146fbc7f8bbd8e0382e30f68e
Glitch (first-enormous-microwave) <none>
2020-03-15 13:33:52 +0000

    🎾🐁 Checkpoint ./komutlar/ban.js:7250430/186 ./komutlar/tagsay.js:7250430/74

--- komutlar/tagsay.js
+++ komutlar/tagsay.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
   var tagdakiler = 0;
-  let tag = "ꏪ";
+  let tag = "†";
   message.guild.members.forEach(member => {
     if(member.user.username.includes(tag)) {
       tagdakiler = tagdakiler+1
     }
   })
-  message.channel.send("<a:kalp:688079879090208779> Tagımızda ` " + tagdakiler + " ` Üye Var. <a:kalp:688079879090208779>")
+  message.channel.send("<a:kalp:688697175407984662> Tagımızda ` " + tagdakiler + " ` Üye Var. <a:kalp:688697175407984662>")
   const Discord = require('discord.js');
 
 
