4fb6986f3c4c203545fee3f5ea52a83ec874891f
Glitch (first-enormous-microwave) <none>
2020-03-16 15:25:57 +0000

    🌯🍝 Checkpoint ./komutlar/sunucu.js:7250430/1004

---
+++ komutlar/sunucu.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+    const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
+    let count = 0;
+    for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
+
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .addField("<a:tik1:688697405708828706> Sunucudaki üye sayısı", message.guild.memberCount)
+        .addField("<a:tik1:688697405708828706> Çevrimiçi üye sayısı", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
+        .addField("<a:tik1:688697405708828706> Seslideki üye sayısı", count)
+        .setFooter(`${message.author.tag} tarafından istendi`, message.author.avatarURL)
+    message.channel.send(embed);
+
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['sayı'],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'say',
+    description: 'Say',
+    usage: 'say'
+};
