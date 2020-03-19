74c85e11a1035b1ee4cf9f1737455c4894f081be
Glitch (first-enormous-microwave) <none>
2020-03-19 22:52:24 +0000

    ⚓️🚤 Checkpoint ./komutlar/sunucu.js:7250430/125

--- komutlar/sunucu.js
+++ komutlar/sunucu.js
@@ -7,11 +7,11 @@ exports.run = async (client, message, args) => {
     for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
 
     const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .addField("Sunucudaki üye sayısı", message.guild.memberCount)
-        .addField("Çevrimiçi üye sayısı", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
-        .addField("Seslideki üye sayısı", count)
-        .addField("Tagdaki üye sayısı", message.guild.members.filter(m => m.user.username.includes(tag)).size) // tagınız yoksa bu satrı silin
+        .setColor("#000001")
+        .addField("<a:tik1:688697405708828706> Sunucudaki üye sayısı", message.guild.memberCount)
+        .addField("<a:tik1:688697405708828706> Çevrimiçi üye sayısı", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
+        .addField("<a:tik1:688697405708828706> Seslideki üye sayısı", count)
+        .addField("<a:tik1:688697405708828706> Tagdaki üye sayısı", message.guild.members.filter(m => m.user.username.includes(tag)).size) // tagınız yoksa bu satrı silin
         .setFooter(`${message.author.tag} tarafından istendi`, message.author.avatarURL)
     message.channel.send(embed);
 
