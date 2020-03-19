3dd5054c77cc3b75d365f2dda23068cf9fec2557
Glitch (first-enormous-microwave) <none>
2020-03-19 23:16:23 +0000

    🌪🏦 Checkpoint ./komutlar/sunucu.js:7250430/3 ./komutlar/ekranaç.js:7250430/684

--- komutlar/sunucu.js
+++ komutlar/sunucu.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args) => {
 
     const embed = new Discord.RichEmbed()
         .setColor("#000001")
-        .addField("<a:tik1:688697405708828706> Sunucudaki üye sayısı", message.guild.memberCount)
+          .addField("<a:tik1:688697405708828706> Sunucudaki üye sayısı", message.guild.memberCount)
         .addField("<a:tik1:688697405708828706> Çevrimiçi üye sayısı", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
         .addField("<a:tik1:688697405708828706> Seslideki üye sayısı", count)
         .addField("<a:tik1:688697405708828706> Tagdaki üye sayısı", message.guild.members.filter(m => m.user.username.includes(tag)).size) // tagınız yoksa bu satrı silin
@@ -30,4 +30,4 @@ exports.help = {
     name: 'say',
     description: 'Say',
     usage: 'say'
-};
+}; 
