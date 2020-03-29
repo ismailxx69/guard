9cfb0675fd7b4d3e95ebd0d925f7d73a0a71f7a4
Glitch (thinkable-lake-fuel) <none>
2020-03-29 14:46:46 +0000

    🍐🏪 Checkpoint ./komutlar/erkek.js:7250430/5396

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -1,45 +1,34 @@
 const Discord = require('discord.js');
-const db = require('quick.db');
-
+const db = require("quick.db")
 
 exports.run = async (client, message, args) => {
 
-  let hata = new Discord.RichEmbed()
-  .setColor(`#000001`)
-  .setDescription(`Bu komutu kullanabilmek için <@&689930799964880946> rolüne sahip olmalısın.`)
-  .setFooter(`${message.author.tag}`, message.author.avatarURL)
-    if (!message.member.roles.has('689930799964880946')) return message.channel.send(hata)(() => message.react('680049992538259468'));
-
-  let kişi = message.mentions.users.first()
-  if (!kişi) return message.react('')
-  let member = message.guild.member(kişi)
-
-  if(!member.roles.has(`689930799964880946`)) { //layıtkı
-    member.addRole(`689930803215335514`) //kayıtlı 
-    member.removeRole(`689930804301791276`) //kayıtsız
-    let kayıt = new Discord.RichEmbed()
-    .setColor(`#000001`)
-    .setDescription(`${kişi} adlı kullanıcı <@&689930803215335514> olarak kaydedildi.`)
-    .setFooter(`${message.author.tag}`, message.author.avatarURL)
-    return message.channel.send(kayıt)(() => message.react('680049992731459604'));
-  } else {
-    member.addRole(`689930804301791276`) // kayıtsız
-    member.removeRole(`689930803215335514`) //kayıtlı
-    let kayıt = new Discord.RichEmbed()
-    .setColor(`#000001`)
-    .setDescription(`${kişi} adlı kullanıcının <@&689930804301791276> kaydı geri alındı.`)
-    .setFooter(`${message.author.tag}`, message.author.avatarURL)
-    return message.channel.send(kayıt)(() => message.react('680049992731459604'));
-  }
+if(!message.member.roles.has("689930799964880946")) return message.channel.send(`Bu komutu kullanabilmek için <@&689930799964880946> yetkisine sahip olmasınız. <a:unlem:689081736088649768>`);
+  let isim = args[1];
+  let yas = args[2];
+  let kullanıcı = message.mentions.users.first()
+  if (!kullanıcı) return message.channel.send('Kullanıcıyı etiketlemeyi unuttun kanka.  <a:unlem:689081736088649768>')
+  if (!isim) return message.channel.send('İsim Yazmayı Unuttun Kanka <a:unlem:689081736088649768>')
+  if (!yas) return message.channel.send('Yaş Yazmayı Unuttun Kanka <a:unlem:689081736088649768>')
+  let rol = message.mentions.roles.first()
+  let member = message.guild.member(kullanıcı)      
+  member.setNickname(`ꏪ ${isim} • ${yas}`)
+  member.addRole('689930803215335514')
+  member.removeRole('689930804301791276')
+  let embed = new Discord.RichEmbed()
+  message.react("689080340123484161")
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
+  kategori: "KULLANICI KOMUTLARI",
   permLevel: 0
-};
+}
 
 exports.help = {
   name: 'erkek',
-};
+  description: "Sunucuya kaydolmaya ne dersin ?",
+  usage: 'kayıt isim yaş'
+}
