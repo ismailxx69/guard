92249efd7e45d60fc9d0faa119e746e8be109cb1
Glitch (thinkable-lake-fuel) <none>
2020-04-03 04:12:43 +0000

    🐝🍣 Checkpoint ./komutlar/vipver.js:7250430/1253

---
+++ komutlar/vipver.js
@@ -0,0 +1,53 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+const ms = require("ms");
+const a = require("../ayarlar.json");
+const client = new Discord.Client();
+exports.run = async (bot, message, args) => {
+  
+  let owners = ['650689514842619914']
+if(!owners.includes(message.author.id)) return message.channel.send(' `Bu Komutu Sadece Sahibim Kullanabilir`');
+
+  
+  
+ let kullanıcı = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  
+
+  if (!kullanıcı) return message.channel.send(' Kullanıcının İdsini Veya Etiketini Girmelisin')
+ 
+
+   
+  
+
+  
+ if(message.guild.members.has(kullanıcı)) message.guild.members.get(kullanıcı).addRole('695477390129758298');
+    
+
+  
+ 
+
+  let byembed1 = new Discord.RichEmbed()
+  .setTitle("Bilgilendirme")
+    .setColor("RED")
+    .setFooter("৳ Chilledia")
+    .setDescription(` ${kullanıcı} Adlı Kişi  Vip Rolü  Verildi** \n Vip Rolü  Veren Kişi ; ${owners}`)
+     
+  
+  
+  message.channel.send(byembed1) 
+  
+   
+    
+
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["vip-ver"],
+  permLevel: 0
+};
+exports.help = {
+  name: 'vip',
+  description: 'goldver',
+  usage: 'goldver'
+};
