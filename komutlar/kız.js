14746312b2036d74d91da541afd8190f89363c84
Glitch (thinkable-lake-fuel) <none>
2020-04-03 16:43:48 +0000

    ⛽️🍚 Checkpoint ./komutlar/kız.js:7250430/2128 ./komutlar/erkek.js:7250430/2342

--- komutlar/kız.js
+++ komutlar/kız.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
 module.exports.run = async (bot, message, args, member, client, level) => {
-  if(!message.member.roles.has("Kayıtçı Rol İd")) return message.channel.send(`**Kusura Bakma Dostum Buna Yetkin Yok.** `);
+  if(!message.member.roles.has("689930799964880946")) return message.channel.send(`**Kusura Bakma Dostum Buna Yetkin Yok.** `);
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!user) return message.reply("**Etiket Atmayı Unuttun!**");
-  user.addRole('Verecegi Rol İd') 
-  user.removeRole('Alacağı Rol İd') 
+  user.addRole('695032377919143946') 
+  user.removeRole('695025904350658620') 
 const ky = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`${user}, **kullanıcısına başarı ile kız rolü verildi**`)
+        .setDescription(`${user}, **kullanıcısına <@&695032377919143946>  rolü verildi**`)
         .setColor('BLACK')
         .setTimestamp()
         message.channel.send(ky)
