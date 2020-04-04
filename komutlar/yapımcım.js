71e81d0ed93203ca20296822c9d58fa572951d02
Glitch (thinkable-lake-fuel) <none>
2020-04-04 23:14:16 +0000

    🎤🐇 Checkpoint ./komutlar/yapımcım.js:7250430/951

---
+++ komutlar/yapımcım.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setColor('BLACK')
+    .setTimestamp()
+    .addField('<a:mor:696120357370986606> **Yapımcım: **', '<a:dvlpr:696119046273237052> <@650689514842619914> \n<a:dvlpr:696119046273237052>') // YAPIMCI
+    message.channel.sendEmbed(ozelmesajkontrol) }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['yapımcım'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yapımcım',
+  description: 'Yapimcimi Gosterir.',
+  usage: 'yapimcim'
+};
