1d23c558a4d981b593189838541e948f9a0450ed
Glitch (thinkable-lake-fuel) <none>
2020-04-07 00:04:52 +0000

    🌷🏅 Checkpoint ./ayarlar.json:13688942/98 ./komutlar/isim.js:13688942/429

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -1,22 +1,21 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-if  (!message.member.roles.has("695788842455793736")) return message.reply('Bu komutu kullanmaya yetkin yok');
+if  (!message.member.roles.has("693660564173357116")) return message.reply('Bu komutu kullanmaya yetkin yok');
   
   let member = message.mentions.members.first();
   let isim = args[1];
-  let yaş = args[2];
-  let tag = "✧"
+  let tag = "√"
   if (!member) return message.channel.send("**Bir Üye Etiketlemelisiniz**");
   if (!isim) return message.channel.send("**Bir İsim Yazmalısınız**");
-  if (!yaş) return message.channel.send("**Bir Yaş Yazmalısınız**");
-  member.setNickname(`${tag} ${isim} | ${yaş}`);
+  member.setNickname(`${tag} | ${isim} |`);
   const embed = new Discord.RichEmbed()
     .setColor('#000001')
     .addField(
-      `**<a:tik:695802304879657072> İsim Başarıyla Değiştirildi **`,
-      `\n**<a:tik:695802304879657072> İsmi Değiştirilen Kullanıcı:** ${member.user} \n **<a:tik:695802304879657072> Yeni Kullanıcı Adı:** \`${tag} ${isim} | ${yaş}\``
+      `**<a:siyah:694927370292822090> İsim Başarıyla Değiştirildi **`,
+      `\n**<a:siyah:694927370292822090> İsmi Değiştirilen Kullanıcı:** ${member.user} \n **<a:siyah:694927370292822090> Yeni Kullanıcı Adı:** \`${tag} ${isim} | \``
     )
+      .setImage("https://cdn.discordapp.com/attachments/693777915179040778/695255047197687858/giphy.gif")
     .setFooter(`Talador`)
     .setThumbnail(client.user.avatarURL);
   message.channel.send(embed);
