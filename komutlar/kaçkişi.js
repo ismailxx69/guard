c85ee0a4815ca059d9625ac5e8038e5c21db9bef
Glitch (thinkable-lake-fuel) <none>
2020-04-08 04:30:54 +0000

    🐓🚞 Checkpoint ./komutlar/kaçkişi.js:13688942/88

--- komutlar/kaçkişi.js
+++ komutlar/kaçkişi.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 
 exports.run = async (client, message, params) => {  
   
-message.channel.send('<a:emoji_12:689079948274958339> Sunucumuzda __'+message.guild.memberCount+'__ Kişi Vardır')
+message.channel.send('<a:kng:695305040096919554> **Sunucumuzda __'+message.guild.memberCount+'__ Kişi Vardır** <a:kng:695305040096919554>')
 
 };
 
