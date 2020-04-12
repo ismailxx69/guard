5b476dbce092d8c6e42b52a396475f9079eb5837
Glitch (free-jasper-coriander) <none>
2020-04-12 04:32:07 +0000

    💨🛣 Checkpoint ./komutlar/afk.js:13688942/430 ./komutlar/rolbilgi.js:13688942/1771

--- komutlar/afk.js
+++ komutlar/afk.js
@@ -4,14 +4,9 @@ const db = require('quick.db');
 
 exports.run = async(client, message, args) => {
 
-if(!args[0]) return message.channel.send('Lütfen afk olma sebebinizi yazınız.')
-   let member = message.mentions.members.first();
-  let isim = args[1];
-  let tag = "√"
-  if (!member) return message.channel.send("**Bir Üye Etiketlemelisiniz**");
-  if (!isim) return message.channel.send("**Bir İsim Yazmalısınız**");
-  member.setNickname(`${tag} • ${isim} `);
-  const embed = new Discord.RichEmbed() 
+if(!args[0]) return message.channel.send('Lütfen afk olma sebebinizi yazınız.') 
+  let tag = "[AFK]"
+  
   message.channel.send("``" + args[0] + "`` Sebebi ile afk oldunuz!")
   db.set(`afks_${message.author.id}`, args[0])
 
