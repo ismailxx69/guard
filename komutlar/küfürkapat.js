657a03357d2d14aaf73ba59d2a37abf1daaa89bc
Glitch (free-jasper-coriander) <none>
2020-04-13 19:38:35 +0000

    👆👻 Checkpoint ./komutlar/küfürengelle.js:13688942/108 ./komutlar/küfürkapat.js:13688942/863 ./bot.js:13688942/21788

--- komutlar/küfürkapat.js
+++ komutlar/küfürkapat.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const db = require('quick.db')
 exports.run = (client, message, args) => { 
       if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(` Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-  if (db.fetch(`küfürE_${message.channel.id}`)) {
-  return message.reply(`**Küfür Engel Zaten Bu Kanalda Aktif!**  ${client.emojis.get("693615832776835162")}`)
+  if (!db.fetch(`küfürE_${message.channel.id}`)) {
+  return message.reply(`**Küfür Engel Zaten Bu Kanalda Kapalı!**  ${client.emojis.get("693615832776835162")}`)
 }
-  db.set(`küfürE_${message.channel.id}`, "aktif")
-  message.reply(`**Bu Özellik Sadece Bu Kanalda Aktif!** ${client.emojis.get("693615832776835162")}`)
+  db.delete(`küfürE_${message.channel.id}`)
+  message.reply(`**Küfür Engel Özelliği Başarılı Bir Şekilde Bu Kanalda Kapatıldı** ${client.emojis.get("693615832776835162")}`)
 };
 exports.conf = {
   enabled: true,  
@@ -16,7 +16,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'küfüraç',
+  name: 'küfürkapat',
   description: 'sayaç', 
   usage: 'sayaç'
 };
