053a076a8d1145f09c4fd49d004a7888dbf65f1f
Glitch (free-jasper-coriander) <none>
2020-04-15 03:25:16 +0000

    🚍☄️ Checkpoint ./ayarlar.json:7250430/84:13688942/59 ./komutlar/süreli-jail.js:13688942/4002

--- komutlar/süreli-jail.js
+++ komutlar/süreli-jail.js
@@ -1,50 +1,36 @@
-const Discord = require("discord.js");
-const ms = require("ms");
-const db = require("quick.db");
+const Discord = require('discord.js');
 
+let rolid = "698066204673310771";  //cezalı rol id
 exports.run = async (client, message, args) => {
-  if(!message.member.roles.has('699805708421431298')) return message.reply('**Bu komutu kullanabilmek için 🛇 | Jail Hammer yetkisine sahip olmasınız.**'); // ID yazan yere komutu kullanabilecek rolün ID
-  let yashinu = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!yashinu) return message.channel.send(`Bir Kullanıcıyı Etiketlemelisin Kanka.`).then(x => x.delete(10000))
-  let member = message.mentions.members.first()
-
-  let rol = message.guild.roles.get("698066204673310771"); // Buraya Cezalı rolünün ID
-  let kayıtsızRolü = message.guild.roles.get("696861671225950310"); // Buraya cezalıdan çıkınca vereceği rolün ID
-  let logYashinu = "699811724253069403"; // loglanacağı kanalın ID
-  let süre = args[1]
-  if (!süre) return message.reply("Süre belirtmelisin.")
-  let sebep = args.slice(2).join(' ')
-  if(!sebep) return message.channel.send(`**Bir Sebep Girmelisin.**`)
-  let sürems = args.slice(1).join(' ').replace('gün'.toLowerCase(), 'd').replace('saat'.toLowerCase(), 'h').replace('dakika'.toLowerCase(), 'm').replace('saniye'.toLowerCase(), 's');
-  const embed = new Discord.RichEmbed().setFooter(message.author.tag , message.author.avatarURL);
-    await(yashinu.addRole(rol.id));
-    yashinu.roles.filter(x => x.id !== rol.id).forEach(async a => await yashinu.removeRole(a.id));
-    yashinu.setNickname(yashinu.user.username);
-    embed.setDescription(`${yashinu}  adlı kullanıcı **__"${sebep}"__** sebebiyle Cezalıya Atıldı \n Zamanı: **${süre}**`)
-    .setColor('RANDOM')
-    .setThumbnail("https://media.giphy.com/media/SuC0zYYuX2GH96YdW6/giphy.gif")
-     message.react("697951599661678654")
-   const embed2 = new Discord.RichEmbed()
-  .setColor(`RANDOM`)
-  .setDescription(`${message.guild.name} Sunucusundan __**${sebep}**__ Sebebiyle Süreli Cezalıya Atıldın.`)
-   member.send(embed2);
-    client.channels.get(logYashinu).send(embed)
-    setTimeout(function(){
-    yashinu.removeRole(rol.id);
-    yashinu.addRole(kayıtsızRolü);
-    }, ms(süre));
-  };
-
+  if (!message.member.roles.has("698887456275300422"))
+    return message.channel.send(
+      `Bu komutu kullanabilmek için <@&698887456275300422> yetkisine sahip olmasınız!`
+    );
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member) return message.channel.send("Hapishaneden Alınacak Üyeyi Etiketleyin!");
+    if(rolid.match(/(\d{17,19})/g)) {
+        member.roles.forEach(role => member.removeRole(role));
+        member.addRole(rolid);
+    }
+    else member.roles.forEach(role => member.removeRole(role));
+        const embed  = new Discord.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/550531815249805312/678679430314459207/index.jpg")
+    .setAuthor('Jail Operasyonu')
+    .setDescription(` **Kullanıcının Tüm Rolleri Alındı Ve <@&698066204673310771> Rolü Verildi**`)
+    .setFooter(`Komutu Kullanan (${message.author.username})`)
+    .setColor("RED")
+    message.react('696861671225950310')
+    return message.channel.sendEmbed(embed);
+}
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ['jail'],
-  permLevel: 0
+    enabled: true,
+    guildOnly: true,
+    aliases: ["jail"],
+    permLevel: 0
 };
 
 exports.help = {
-  name: 'jail',
-  description: 'Belirtilen kullanıcıyı belirtilen süre kadar cezalıya atar.',
-  usage: 'cezalı @Kullanıcı [İsterseniz Süre]',
-  kategori: 'yetkili'
+    name: "jail",
+    description: 'Birini jaillersiniz.',
+    usage: 'jail <kullanıcı>'
 };
