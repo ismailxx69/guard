4f6b24c416071c63dd8fed966b53c7f1b76a2b61
Glitch (free-jasper-coriander) <none>
2020-04-16 09:39:49 +0000

    🎫🎗 Checkpoint ./komutlar/isim.js:13688942/422 ./.glitch-assets:13688942/483

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -11,11 +11,15 @@ if  (!message.member.roles.has("700144704578125920")) return message.reply('Bu k
   member.setNickname(`${tag}  ${isim} `);
   const embed = new Discord.RichEmbed()
     .setColor('#000001')
-    .addField(
+ .setImage(`https://www.bobiler.org/monte/preview/116199/bobiler.gif`)
+  .addField(
       `**<a:onay:700188249330810910> İsim Başarıyla Değiştirildi **`,
       `\n**<a:onay:700188249330810910> İsmi Değiştirilen Kullanıcı:** ${member.user} \n **<a:onay:700188249330810910> Yeni Kullanıcı Adı:** \`${tag}  ${isim}  \``
     )
-    .setFooter(`Talador`)
+  
+  
+  
+    .setFooter(`ㄨΛURORΛ`)
     .setThumbnail(client.user.avatarURL);
   message.channel.send(embed);
 };
