a5cf4b41d8fb264a0dc44dead62be472931c9b01
Glitch (aurora-kayit-bot) <none>
2020-04-21 17:09:44 +0000

    🙅🌆 Checkpoint ./komutlar/isim.js:13688942/147 ./komutlar/kız.js:13688942/2 ./komutlar/erkek.js:13688942/484

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args, member, client, level) => {
   user.removeRole('700144704506953832') 
 const ky = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`<a:krmzn:700970912039829544> ${user}, **  kullanıcısının kayıt işlemi başarı ile yapıldı.**  `)
+        .setDescription(`<a:onay:700188249330810910> **Kayıt işlemi sonucu** \n\n <a:krmzn:700970912039829544> ${user}, **  kullanıcısının ismi başarıyla değiştirildi. **  \n  `)
          .setThumbnail("https://media.giphy.com/media/JQvFzbikYei9fETSL2/giphy.gif")
         .setColor('BLACK')
         .setTimestamp()
