dbabd29e830947e4ea73f394cd8a6d1cadd684d0
Glitch (aurora-kayit-bot) <none>
2020-04-21 05:25:48 +0000

    🍛💑 Checkpoint ./komutlar/kaçkişi.js:13688942/457

--- komutlar/kaçkişi.js
+++ komutlar/kaçkişi.js
@@ -3,6 +3,16 @@ const ayarlar = require('../ayarlar.json');
 const db = require('quick.db');
 const moment = require('moment');
 
+exports.run = (client, message, args) => {
+  var tagdakiler = 0;
+  let tag = "ㄨ";
+  message.guild.members.forEach(member => {
+    if(member.user.username.includes(tag)) {
+      tagdakiler = tagdakiler+1
+    }
+  })
+  message.channel.send("  **Tagımızda __` " + tagdakiler + " `__ Üye Var.** ")
+
 exports.run = async (client, message, params) => {  
   
 message.channel.send(' **<a:onay:700188249330810910> Sunucumuzda  __'+message.guild.memberCount+'__ Kişi Vardır** ')
@@ -17,7 +27,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'kk',
+  name: 'say',
   description: 'Sunucunun bilgilerini gönderir.',
   usage: 'kaç-kişiyiz'
 }; 
