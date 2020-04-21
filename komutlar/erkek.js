46c33ce96ec1a17d8cf49eca903d1e97187eea65
Glitch (aurora-kayit-bot) <none>
2020-04-21 05:36:50 +0000

    💸🎍 Checkpoint ./komutlar/kaçkişi.js:13688942/874 ./komutlar/erkek.js:13688942/279 ./bot.js:13688942/4

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -4,11 +4,12 @@ module.exports.run = async (bot, message, args, member, client, level) => {
   if(!message.member.roles.has("700144704578125920")) return message.channel.send(`**Kusura Bakma Dostum Buna Yetkin Yok.** `);
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!user) return message.reply("**Etiket Atmayı Unuttun!**");
+  user.addRole('701400791797334097') 
   user.addRole('700144704565673990') 
   user.removeRole('700144704506953832') 
 const ky = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`${user}, ** <a:krmzn:700970912039829544> kullanıcısına <@&700144704565673990> rolü verildi.** `)
+        .setDescription(`<a:krmzn:700970912039829544> ${user}, **  kullanıcısının ayıt işlemi başarı ile yapıldı.** \n Sunucumuz seninle birlikte `)
 
         .setColor('BLACK')
         .setTimestamp()
--- komutlar/kaçkişi.js
+++ komutlar/kaçkişi.js
@@ -2,17 +2,7 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 const db = require('quick.db');
 const moment = require('moment');
-
-exports.run = (client, message, args) => {
-  var tagdakiler = 0;
-  let tag = "ㄨ";
-  message.guild.members.forEach(member => {
-    if(member.user.username.includes(tag)) {
-      tagdakiler = tagdakiler+1
-    }
-  })
-  message.channel.send("  **Tagımızda __` " + tagdakiler + " `__ Üye Var.** ")
-
+  
 exports.run = async (client, message, params) => {  
   
 message.channel.send(' **<a:onay:700188249330810910> Sunucumuzda  __'+message.guild.memberCount+'__ Kişi Vardır** ')
