7b472ca48f1e33a33b9524c19eb72f18f566ab3a
Glitch (aurora-kayit-bot) <none>
2020-05-02 16:37:51 +0000

    🛫🍍 Checkpoint ./komutlar/geçici-sustur.js:13688942/36

--- komutlar/geçici-sustur.js
+++ komutlar/geçici-sustur.js
@@ -4,7 +4,7 @@ const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
   // 1s = 1 saniye , 1m = 1 dakika , 1h = 1 saat, 1d = 1 gün
 
-     if(!message.member.roles.has("704647763064782898")) return message.channel.send(`**Kusura Bakma Dostum Buna Yetkin Yok.** `);
+     if(!message.member.roles.has("700144704607617038")) return message.channel.send(`**Kusura Bakma Dostum Buna Yetkin Yok.** `);
     
 
   let ownerkod = message.guild.member(
