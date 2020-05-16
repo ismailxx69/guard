5f91f96f0a39ef5aab81801ca746e81307123f01
Glitch (aurora-kayit-bot) <none>
2020-05-16 11:34:06 +0000

    🌲🐦 Checkpoint ./komutlar/çek.js:14121940/1239

---
+++ komutlar/çek.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js"); //Pyro tarafından kodlanıldı çalınması taktirinde muck
+exports.run = (client, message, args) => { //Pyro tarafından kodlanıldı çalınması taktirinde muck
+    if (!message.member.hasPermissions("MOVE_MEMBERS")) return message.channel.send(":no_entry: Bu komutu kullana bilmek için `Üyeleri taşı` yetkisine sahip olmanız gerek")
+    let kanal = args[1];
+    let kullanici = message.mentions.members.first() //Pyro tarafından kodlanıldı çalınması taktirinde muck
+    if (!kanal) return message.channel.send("Kanal belirtmedin")
+    if (!kullanici) return message.channel.send("Kullanıcıyı belirtmedin") //Pyro tarafından kodlanıldı çalınması taktirinde muck
+    kullanici.setVoiceChannel(`${kanal}`)
+        .then(() =>
+            message.channel.send(`${kullanici} <#${kanal}> adlı kanala taşındı`))
+        .catch(console.error);
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ['üyeyitaşı'],
+    permLevel: 0
+};
+exports.help = { //Pyro tarafından kodlanıldı çalınması taktirinde muck
+    name: 'taşı',
+    description: 'İstediğiniz kişiniyi bir sesli kanaldan diğerine taşır.', //Pyro tarafından kodlanıldı çalınması taktirinde muck
+    usage: 'taşı [kullanıcı] [kanal id]'
+};
