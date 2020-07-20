304c53b11578e4673f5b9a581945def9f0f4b86a
Glitch (caramel-wakeful-cast) <none>
2020-07-20 05:49:37 +0000

    📹🍖 Checkpoint ./events/ready.js:16871481/22 ./ayarlar.json:16871481/95 ./komutlar/taşı.js:16871481/36 ./komutlar/çek.js:16871481/2582

--- komutlar/çek.js
+++ komutlar/çek.js
@@ -1,52 +1,30 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+exports.run = async (client, message, args ) => {
+  if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send("Bu Komutu Kullanabilmek İçin **Üyeleri Taşı** Yetkisine Sahip Olman Gerek!")
 
-exports.run = async (client, message, args) => {
+ let seslikanal = message.member.voiceChannelID
+let seslikanal1 = message.member.voiceChannel
+let kullanıcı = message.mentions.members.first()
+let kullanıcıkanal = kullanıcı.voiceChannel
+if(!seslikanal) return message.channel.send("Bir Sesli Kanalda Değilsin!")
+if(kullanıcı.id == message.member.id) return message.channel.send("Kendini Çekemezsin!")
+if(!args[0]) return message.channel.send("Birini Etiketlemelisin!")
+if(!kullanıcıkanal) return message.channel.send("Etiketlediğin Kullanıcı Sesli Kanalda Değil!")
 
-  if (!message.member.hasPermission("685555226325024829")) return message.channel.send(`Bu komutu kullanmaya yetkin yok!`);
+if(kullanıcıkanal) {
+kullanıcı.setVoiceChannel(seslikanal)
+message.channel.send(`${kullanıcı} Başarıyla **${kullanıcıkanal}** Kanalından **${seslikanal1}** Kanalına Çekildi!`)
+}
+ }
 
-  if (!message.member.voiceChannel) {
-    return message.channel.send("Ses kanalında olman lazım!");
-  }
-
-  let kullanıcı = message.mentions.members.first();
-  if (!kullanıcı) return message.channel.send("**Kullanıcıyı etiketlemelisin.**");
-
-  let rol = message.mentions.roles.first();
-  let member = message.guild.member(kullanıcı);
-
-  if (!member.voiceChannel) return message.channel.send("Etiketlenen kullanıcı bir ses kanalında değil").then(m => m.delete(5000));
-
-  const voiceChannel = message.member.voiceChannel.id;
-  if (!voiceChannel) return;
-
-  member.setVoiceChannel(voiceChannel);
-
-  const voiceChannel1 = message.member.voiceChannel.name;
-  let embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setDescription(
-      message.author +
-        " **Tarafından** " +
-        kullanıcı +
-        " **Kullanıcısı** `" +
-        voiceChannel1 +
-        "`** Sesli Kanalına Çekildi.**"
-    )
-    .setFooter(
-      `${message.author.tag}`,
-      `${message.author.displayAvatarURL}`
-    )
-    .setTimestamp();
-  message.channel.send(embed).then(m => m.delete(5000));
-};
 exports.conf = {
   enabled: true,
-  guildOnly: true,
-  aliases: ["çek"],
+  guildOnly: false,
+  aliases: [],
   permLevel: 0
 };
+
 exports.help = {
-  name: "çek",
-  description: "çek",
-  usage: "çek"
+  name: 'çek',
+  açıklama: ''
 };
