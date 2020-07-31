3067e9ecb180137d1dab34b70b5e59473428b9e5
Glitch (caramel-wakeful-cast) <none>
2020-07-31 13:35:05 +0000

    🏟🐆 Checkpoint ./komutlar/rolver.js:16871481/96

--- komutlar/rolver.js
+++ komutlar/rolver.js
@@ -17,7 +17,7 @@ exports.run = async (bot, message, args) => {
 
     if (rMember.roles.has(aRole.id)) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Bu kullanıcı zaten bu rolde.').setColor(10038562));
     await (rMember.addRole(aRole.id))
-    message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`<a:siyah:711280680889483345> ${rMember} **isimli üyeye \`${role.name}\` isimli yetki başarıyla verildi!** <a:siyah:711280680889483345> `).setColor('RANDOM'));
+    message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`<a:sytik:729283049090711563> ${rMember} **isimli üyeye \`${role.name}\` isimli yetki başarıyla verildi!** <a:sytik:729283049090711563> `).setColor('RANDOM'));
 
 };
 
