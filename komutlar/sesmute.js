5952ad8e7e8d3cda1be9473840c9e2db6b65a654
Glitch (reminiscent-rapid-fossa) <none>
2020-08-07 17:51:21 +0000

    👚🚄 Checkpoint ./komutlar/sesmute.js:16871481/2699

--- komutlar/sesmute.js
+++ komutlar/sesmute.js
@@ -1,35 +1,36 @@
 const Discord = require('discord.js');
-const ms = require("ms");
+const ayarlar = require('../ayarlar.json');
+const ms = require('ms');
 
-exports.run = (client, message, args) => {
-        if (!message.member.hasPermissions("729271900077031504")) return message.channel.send(":no_entry: Bu komutu kullana bilmek için `Move` yetkisine sahip olmanız gerek")
-  if (!message.member.hasPermissions("MUTE_MEMBERS")) return message.channel.send("Bu komutu kullanabilmek için `Üyeleri sustur` yetkisine sahip olmanız gerek.")
-    
-  let kullanici = message.mentions.members.first()
-    
-    
-    let süre = args[1]
-    if (!süre) return message.reply("Süre belirtmelisin.")
-    if (!kullanici) return message.channel.send("Kimi susturacağını belirtmedin.")
-    kullanici.setMute(true, `Susturan yetkili: ${message.author.tag} - Susturma süresi: ${süre}ms`)
-        .then(() =>
-            message.channel.send(`${kullanici} \`${süre}\`**ms** ses kanallarında susturuldu.`))
-        .catch(console.error);
-        setTimeout(() => {
-
-        kullanici.setMute(false,`Süresi dolduğu için susturması kaldırıldı.`)
-        message.channel.send(`${kullanici} Süresi dolduğu için mikrafonu açıldı. `)
-
-    }, ms(süre))
-}
-
-exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: ["sesli-sustur"],
-    permLevel: 0
+exports.run = async(client, message, args, prefix, ayar, emoji) => {
+  // !sesmute @etiket 5m
+  let uye = message.mentions.members.first();
+  let sure = args[1];
+  let sebep = args.slice(2).join(' ');
+  let logKanali = "735861880115167283"; // LOG KANALININ ID
+  if (!uye || !sure || !sebep) return message.reply('Doğru kullanmalısın!\n' +this.help.usage);
+  if (!uye.voiceChannel) return message.reply('Kişi bir ses kanalda değil!');
+  if (!uye.voiceChannel.permissionsFor(message.author).has('MUTE_MEMBERS')) return message.reply('Bunu yapman için yeterli izinlere sahip değilsin!');
+  uye.setMute(true);
+  message.channel.send(`${uye.displayName} adlı üye ses kanalında **${sure}** kadar susturuldu!`);
+  client.channels.get(logKanali).send(`${uye} adlı üye ses kanalında **${sure}** kadar, **${sebep}** nedeniyle susturuldu!`);
+  setTimeout(() => {
+    uye.setMute(false);
+    message.channel.send(`${uye.displayName} adlı üyenin ses susturması kaldırıldı!`);
+    client.channels.get(logKanali).send(`${uye.displayName} adlı üyenin ses susturması kaldırıldı!`);
+  }, ms(sure));
 };
 
-exports.help = {
-    name: 'sesli-sustur'
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = { 
+  name: 'sesmute', 
+  description: 'Sesmute.',
+  usage: 'sesmute @üye süre sebep',
+  kategori: 'kullanıcı'
 };
