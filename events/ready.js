3a5ba27c0a1530d858745ce4449d9c48a478c8d9
Glitch (reminiscent-rapid-fossa) <none>
2020-08-08 17:56:04 +0000

    😅🚅 Checkpoint ./events/ready.js:16871481/13

--- events/ready.js
+++ events/ready.js
@@ -12,7 +12,7 @@ module.exports = client => {
   console.log(` T: ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` users!`);
   client.user.setPresence({
     game: {
-        name: 'lluvia benim her şeyim!',         type: 'WATCHING' // Değerler: // PLAYING: // WATCHING // LISTENING
+        name: 'Selinay benim her şeyim!',         type: 'WATCHING' // Değerler: // PLAYING: // WATCHING // LISTENING
     },
     status: 'online' // Değerler // online // dnd // idle 
 })
