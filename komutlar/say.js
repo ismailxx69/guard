774892ef2a4cf76d71ba3fd375a046b85c4f74ca
Glitch (clean-emphasized-bedbug) <none>
2020-08-09 18:07:46 +0000

    🕷⏰ Checkpoint ./komutlar/say.js:16871481/504

--- komutlar/say.js
+++ komutlar/say.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");//SYLVESTER 35½
 
 exports.run = async (client, message, args) => {
-  let tag = "✩"; // tagınız
+  let tag = "✶"; // tagınız
   const voiceChannels = message.guild.channels.filter(c => c.type === "voice");
   let count = 0;
   for (const [id, voiceChannel] of voiceChannels)
@@ -11,14 +11,14 @@ exports.run = async (client, message, args) => {
 const embed = new Discord.RichEmbed()
     .setAuthor(message.guild.name, message.guild.iconURL) 
     .setThumbnail("https://cdn.discordapp.com/attachments/741633554764660829/741795393116831825/giphy.gif")
-    .setDescription(` <a:star1:741811157240905820>  **Sunucudaki üye sayısı :** __${message.guild.memberCount}__
- <a:star1:741811157240905820>  **Çevrimiçi üye sayısı :** __${message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size}__
- <a:star1:741811157240905820>   **Seslideki üye sayısı :** __${count}__
- <a:star1:741811157240905820>   **Tagdaki üye sayısı :** __${message.guild.members.filter(m => m.user.username.includes(tag)).size}__
- <a:star1:741811157240905820>  **Erkek üye sayısı :** __${message.guild.roles.get("741641798073450496").members.size}__
- <a:star1:741811157240905820>  **Kayıtsız üye sayısı :** __${message.guild.roles.get("741641795867246612").members.size}__
- <a:star1:741811157240905820>  **Kız üye sayısı :** __${message.guild.roles.get("741645637086609481").members.size}__
- <a:star1:741811157240905820>   **Aktif üye sayısı :** __${message.guild.members.filter(m => m.presence.status !== "offline").size}__ ***(Anlık)***
+    .setDescription(` <a:bellatrix13:741652361579069452>  **Sunucudaki üye sayısı :** __${message.guild.memberCount}__
+ <a:bellatrix13:741652361579069452> **Çevrimiçi üye sayısı :** __${message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size}__
+ <a:bellatrix13:741652361579069452> **Seslideki üye sayısı :** __${count}__
+ <a:bellatrix13:741652361579069452> **Tagdaki üye sayısı :** __${message.guild.members.filter(m => m.user.username.includes(tag)).size}__
+ <a:bellatrix13:741652361579069452> **Erkek üye sayısı :** __${message.guild.roles.get("741641798073450496").members.size}__
+ <a:bellatrix13:741652361579069452> **Kayıtsız üye sayısı :** __${message.guild.roles.get("741641795867246612").members.size}__
+ <a:bellatrix13:741652361579069452> **Kız üye sayısı :** __${message.guild.roles.get("741645637086609481").members.size}__
+ <a:bellatrix13:741652361579069452> **Aktif üye sayısı :** __${message.guild.members.filter(m => m.presence.status !== "offline").size}__ ***(Anlık)***
 `)
     .setColor("#005b94")
     .setFooter(`Aha bak bak ne yazmış gel gel!`,message.author.avatarURL);
