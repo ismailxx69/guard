4b86d28c5cbeb55779f9c37eae7ea568be09036c
Glitch (clean-emphasized-bedbug) <none>
2020-08-09 00:57:16 +0000

    ✨🕴 Checkpoint ./bot.js:16871481/1022

--- bot.js
+++ bot.js
@@ -231,24 +231,7 @@ message.reply('  **Aleyküm selam hoş geldin, umarım keyifli bir sohbet olur.*
 
 var uyarilar = {};
 
-client.on("message", async message => {
-  if (!message.guild || !message.member || message.author.bot || !message.content || message.member.hasPermission("ADMINISTRATOR")) return;
-  let mesajIcerik = message.content.replace(/[^a-zA-ZığüşöçĞÜŞİÖÇ]+/g, "");
-  if (mesajIcerik === mesajIcerik.toUpperCase()) {
-    uyarilar[message.author.id] = uyarilar[message.author.id] ? Number(uyarilar[message.author.id])+1 : 1;
-    message.delete(200);
-    message.channel.send(new Discord.RichEmbed().setDescription(`${message.author} büyük harf kullanmamlısın! (${uyarilar[message.author.id]})`));
-    if (uyarilar[message.author.id] >= 5) {
-      message.member.addRole('741641795867246612');
-      message.author.send(`${message.guild.name} sunucusunda fazla büyük harf kullandığın için susturuldun!`);
-      uyarilar[message.author.id] = 0;
-      setTimeout(() => {
-        message.member.removeRole('741641795867246612');
-        message.author.send(`Susturulman bitti artık konuşabilirsin!`);
-      }, 5*60*1000);
-    };
-  };
-});
+
 
 
 
