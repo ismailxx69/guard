ee085963211f7c5e744a01537cbd00f7e38d10a0
Glitch (clean-emphasized-bedbug) <none>
2020-08-10 12:11:05 +0000

    ☂️⛺️ Checkpoint ./bot.js:16871481/244 ./package.json:16871481/1900

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const ayarlar = require("./ayarlar.json");
-
+const googleTTS = require('google-tts-api');
 const chalk = require("chalk");
 const fs = require("fs");
 
